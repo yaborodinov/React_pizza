b00f92bace03ddd9ce004578a44d4d2fa5432b83
Alexandr <yaborodinov@gmail.com>
2021-05-27 11:51:37 +0300

    add fake-server

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
 
 
   React.useEffect(() => {
-    axios.get("http://localhost:3000/db.json").then(({ data }) => {
-      dispatch(setPizzas(data.pizzas))
+    axios.get("http://localhost:3001/pizzas").then(({ data }) => {
+      dispatch(setPizzas(data))
     })
   }, [dispatch])
   return (
